0d369cdf40da2d06b1a41caf949239382cb6ff01
Patricio Reinoso <patricio.reinoso.mendoza@gmail.com>
2024-03-31 21:56:47 -0500

    fix para exportar en static

--- src/app/[year]/[month]/[day]/[slug]/page.tsx
+++ src/app/[year]/[month]/[day]/[slug]/page.tsx
@@ -1,15 +1,36 @@
 // pages/[year]/[month]/[day]/[slug].js
-'use client'
+// 'use client'
 
 import { usePathname } from 'next/navigation';
 import { SinglePost } from '@/components/SinglePost';
 
+export async function generateStaticParams() {
+  const WORDPRESS_API_URL='https://patolin.com/wp-json/wp/v2';
+
+  const posts = await fetch('https://patolin.com/wp-json/wp/v2/posts').then((res) => res.json())
+ 
+  const maps =  posts.map((post: any) => ({
+    year: post.date?.split('T')[0]?.split('-')[0],
+    month: post.date?.split('T')[0]?.split('-')[1],
+    day: post.date?.split('T')[0]?.split('-')[2],
+    slug: post.slug
+  }))
+  return maps;
+}
+
 export default function Post() {
   const pathname = usePathname();
-  const pathnameComponents = pathname.split('/').filter(Boolean);
-  const [year, month, day, slug] = pathnameComponents;
+  
+  if (pathname) {
+    const pathnameComponents = pathname?.split('/').filter(Boolean);
+    const [year, month, day, slug] = pathnameComponents;
+    return (
+        <SinglePost slug={slug} />
+    );
 
+  }
   return (
-      <SinglePost slug={slug} />
-  );
+    <p>Error al validar la URL</p>
+  )
+
 }
